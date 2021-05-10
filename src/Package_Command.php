41bba20966162ed5210bede898f5c94d40f9e6e3
Alain Schlesser <alain.schlesser@gmail.com>
2021-05-10 11:20:50 +0100

    Remove package index example

--- src/Package_Command.php
+++ src/Package_Command.php
@@ -195,29 +195,11 @@ class Package_Command extends WP_CLI_Command {
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     # Install the latest development version from the package index.
-	 *     $ wp package install wp-cli/server-command
-	 *     Installing package wp-cli/server-command (dev-master)
-	 *     Updating /home/person/.wp-cli/packages/composer.json to require the package...
-	 *     Using Composer to install the package...
-	 *     ---
-	 *     Loading composer repositories with package information
-	 *     Updating dependencies
-	 *     Resolving dependencies through SAT
-	 *     Dependency resolution completed in 0.005 seconds
-	 *     Analyzed 732 packages to resolve dependencies
-	 *     Analyzed 1034 rules to resolve dependencies
-	 *      - Installing package
-	 *     Writing lock file
-	 *     Generating autoload files
-	 *     ---
-	 *     Success: Package installed.
+	 *     # Install a package hosted at a git URL.
+	 *     $ wp package install runcommand/hook
 	 *
 	 *     # Install the latest stable version.
 	 *     $ wp package install wp-cli/server-command:@stable
-	 *
-	 *     # Install a package hosted at a git URL.
-	 *     $ wp package install runcommand/hook
 	 *
 	 *     # Install a package hosted at a GitLab.com URL.
 	 *     $ wp package install https://gitlab.com/foo/wp-cli-bar-command.git
